17901411a1a11026c75d0077d312faad70b89f09
Nicholas Illenberger <nillen@pennmedicine.upenn.edu>
2019-05-10 18:33:18 -0400

    Fixed issue with ReturnORR instead of ReturnOOR

--- R/AnalyzeResults.R
+++ R/AnalyzeResults.R
@@ -4,11 +4,12 @@
 #' from many runs of Simulation(), this function outputs a matrix of bias, RMSE, and coverage
 #' probabilities
 #' @param Results A list of results from multiple runs of Simulation()
-#' @param ReturnORR Logical value. Do you want to know how many quantile estimates
+#' @param ReturnOOR Logical value. Do you want to know how many quantile estimates 
 #' were inestimable because the quantile was outside of the range of the data?
 #' @export
 
 AnalyzeResults = function(Results, ReturnOOR = FALSE) {
+  
   CoverageMats = lapply(Results, function(x) x$CoverageMat)
   EstimatedMats = lapply(Results, function(x) x$EstimateMat)
   
